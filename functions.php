ee13e652dbb531c665e41cb359ebc584601b22a0
Amarmani1234 <amaruniworld@gmail.com>
2025-08-14 16:39:54 +0530

    Add header and services page

--- functions.php
+++ functions.php
@@ -280,17 +280,7 @@ add_action('after_setup_theme','university_features');
 //
 function kaddora_enqueue_scripts() {
     $theme_uri = get_template_directory_uri();    
-    wp_enqueue_style('swiper', 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css', array(), null);
-    wp_enqueue_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css', array(), null);
-    wp_enqueue_style('vendor', $theme_uri . '/assets/css/vendor.css', array(), null);
-    wp_enqueue_style('kaddora-style', $theme_uri . '/assets/css/style.css', array(), null);
-    wp_enqueue_style(
-        'google-fonts',
-        'https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap',
-        array(),
-        null
-    );
-    wp_enqueue_script('jquery');
+    wp_enqueue_script('jquery');    
     wp_enqueue_script('popper', $theme_uri . '/assets/bootstrap4/popper.js', array('jquery'), null, true);
     wp_enqueue_script('bootstrap', $theme_uri . '/assets/bootstrap4/bootstrap.min.js', array('jquery', 'popper'), null, true);    
     wp_enqueue_script('isotope', $theme_uri . '/assets/js/isotope.pkgd.min.js', array('jquery'), null, true);
@@ -305,7 +295,22 @@ function kaddora_enqueue_scripts() {
 }
 add_action('wp_enqueue_scripts', 'kaddora_enqueue_scripts');
 
-
+function kaddora_enqueue_styles() {s
+    $theme_uri = get_template_directory_uri();
+    
+    wp_enqueue_style('bootstrap', $theme_uri . '/assets/bootstrap4/bootstrap.min.css');    
+    wp_enqueue_style('font-awesome', $theme_uri . '/assets/css/font-awesome.min.css');
+    wp_enqueue_style('owl-carousel', $theme_uri . '/assets/css/owl.carousel.css');
+    wp_enqueue_style('owl-theme', $theme_uri . '/assets/css/owl.theme.default.css');
+    wp_enqueue_style('animate', $theme_uri . '/assets/css/animate.css');
+    wp_enqueue_style('kaddora-main-style', $theme_uri . '/assets/css/main_styles.css');
+    wp_enqueue_style('kaddora-responsive', $theme_uri . '/assets/css/responsive.css');
+    
+    wp_enqueue_style('kaddora-google-fonts', 'https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap', array(), null);
+    
+    wp_enqueue_style('swiper', 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css');
+}
+add_action('wp_enqueue_scripts', 'kaddora_enqueue_styles');
 //
 function medilab_customize_register($wp_customize) {
 
