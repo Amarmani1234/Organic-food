14fae42475dd5b74d344d33bcc2cba931a950d2f
Chandan Kumar <chandankumar26102000@gmail.com>
2025-08-14 16:54:41 +0530

    kfldsjfs

--- functions.php
+++ functions.php
@@ -295,22 +295,7 @@ function kaddora_enqueue_scripts() {
 }
 add_action('wp_enqueue_scripts', 'kaddora_enqueue_scripts');
 
-function kaddora_enqueue_styles() {s
-    $theme_uri = get_template_directory_uri();
-    
-    wp_enqueue_style('bootstrap', $theme_uri . '/assets/bootstrap4/bootstrap.min.css');    
-    wp_enqueue_style('font-awesome', $theme_uri . '/assets/css/font-awesome.min.css');
-    wp_enqueue_style('owl-carousel', $theme_uri . '/assets/css/owl.carousel.css');
-    wp_enqueue_style('owl-theme', $theme_uri . '/assets/css/owl.theme.default.css');
-    wp_enqueue_style('animate', $theme_uri . '/assets/css/animate.css');
-    wp_enqueue_style('kaddora-main-style', $theme_uri . '/assets/css/main_styles.css');
-    wp_enqueue_style('kaddora-responsive', $theme_uri . '/assets/css/responsive.css');
-    
-    wp_enqueue_style('kaddora-google-fonts', 'https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap', array(), null);
-    
-    wp_enqueue_style('swiper', 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css');
-}
-add_action('wp_enqueue_scripts', 'kaddora_enqueue_styles');
+
 //
 function medilab_customize_register($wp_customize) {
 
