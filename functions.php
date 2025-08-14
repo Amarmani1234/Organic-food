d4b1179a016574f812c5ae46da3eeff664b4790b
Amarmani1234 <amaruniworld@gmail.com>
2025-08-14 15:55:49 +0530

    Add css file link

--- functions.php
+++ functions.php
@@ -280,7 +280,17 @@ add_action('after_setup_theme','university_features');
 //
 function kaddora_enqueue_scripts() {
     $theme_uri = get_template_directory_uri();    
-    wp_enqueue_script('jquery');    
+    wp_enqueue_style('swiper', 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css', array(), null);
+    wp_enqueue_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css', array(), null);
+    wp_enqueue_style('vendor', $theme_uri . '/assets/css/vendor.css', array(), null);
+    wp_enqueue_style('kaddora-style', $theme_uri . '/assets/css/style.css', array(), null);
+    wp_enqueue_style(
+        'google-fonts',
+        'https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap',
+        array(),
+        null
+    );
+    wp_enqueue_script('jquery');
     wp_enqueue_script('popper', $theme_uri . '/assets/bootstrap4/popper.js', array('jquery'), null, true);
     wp_enqueue_script('bootstrap', $theme_uri . '/assets/bootstrap4/bootstrap.min.js', array('jquery', 'popper'), null, true);    
     wp_enqueue_script('isotope', $theme_uri . '/assets/js/isotope.pkgd.min.js', array('jquery'), null, true);
