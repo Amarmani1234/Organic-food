5056081d91aa5dcb61907951cf43726e35c917f6
Chandan Kumar <93089403+chandan578@users.noreply.github.com>
2025-08-16 15:58:01 +0530

    page.php changed..

--- page.php
+++ page.php
@@ -15,23 +15,37 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<main id="primary" class="site-main">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+	<section class="page-content">
+		<?php if (have_posts()): ?>
+			<?php while (have_posts()):
+				the_post(); ?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<h2 class="py-3"><?php the_title(); ?></h2>
+					<div class="entry-content px-4">
+						<?php the_content(); ?>
+					</div>
+				</article>
+			<?php endwhile; ?>
+		<?php endif; ?>
+	</section>
 
-			get_template_part( 'template-parts/content', 'page' );
+	<!-- <?php
+	// while ( have_posts() ) :
+	// 	the_post();
+	
+	// 	get_template_part( 'template-parts/content', 'page' );
+	
+	// 	// If comments are open or we have at least one comment, load up the comment template.
+	// 	if ( comments_open() || get_comments_number() ) :
+	// 		comments_template();
+	// 	endif;
+	
+	// endwhile; // End of the loop.
+	?> -->
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-	</main><!-- #main -->
+</main><!-- #main -->
 
 <?php
 get_sidebar();
